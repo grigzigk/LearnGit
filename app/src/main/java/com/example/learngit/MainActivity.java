d9a55e4f506a39bd836f6c88b155ce381923a68d
grigzigk <gzipos@yahoo.gr>
2022-11-22 08:01:26 +0200

    2nd commit from branch1

--- app/src/main/java/com/example/learngit/MainActivity.java
+++ app/src/main/java/com/example/learngit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //1st message from branch1
+
         //2nd message from branch1
     }
 }
